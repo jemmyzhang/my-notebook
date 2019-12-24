426d2d2ee985f6ff7acd3ba2a0032bff5f2adc6a
Jemmy Zhang <hzdan360@163.com>
2019-12-24 23:56:23 +0800

    update the netty example.
    Signed-off-by: Jemmy Zhang <hzdan360@163.com>

--- src/main/java/pers/jz/netty/example/http/HttpServer.java
+++ src/main/java/pers/jz/netty/example/http/HttpServer.java
@@ -1,7 +1,49 @@
 package pers.jz.netty.example.http;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
 /**
  * @author Jemmy Zhang on 2019/12/22.
  */
 public class HttpServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpServerCodec());
+                            pipeline.addLast(new HttpServerExpectContinueHandler());
+                            pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+                            pipeline.addLast(new HttpHelloWorldServerHandler());
+                        }
+                    });
+
+            Channel ch = bootstrap.bind(8080).sync().channel();
+            System.err.println("Open your web browser and navigate to " +
+                    "http://127.0.0.1:8080");
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
 }
